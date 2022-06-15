4056cce4ddb637478b970e9384f5650cd845eec2
Abdi-Ghasem <ghasem.abdi@yahoo.com>
2022-06-15 00:43:47 -0300

    Initial Commit

---
+++ iPose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPose
+//
+//  Created by Ghasem Abdi on 2022-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
